7f0191b6633ec62a3e7686342b9e12a7f44d2ec0
zhenjess <jessicazhen8@gmail.com>
2019-11-17 09:29:00 -0800

    Create new files

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+const bodyParser = require('body-parser');
